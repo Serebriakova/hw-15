84c3bdba842edece7e659519b236f16a0e704ddb
Serebriakova <serebriakova.alla@gmail.com>
2021-11-08 18:20:14 +0200

    update

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 
-function padString (string, num, glyph, side) {
-    let newString = string;
-    if ( (string === undefined) || (num === undefined) || (glyph === undefined) || (side === undefined)) {
-      return  "Error - at least one argument is missing";
-    } else if ((typeof string !== "string") || (string.trim() === "")) {
-        return "Error - Empty or not string";
-    } else if ( (isNaN(num) || (num === null)) || (num <= 0)) {
-        return "Error - not a number";
-    } else if ((glyph.length === 0) || (glyph.length > 1)) {
-       return "Error - not 1 symbol"
-    } else if (typeof side !== "boolean") {
-        return "Error - not boolean";
-    } 
+function padString(string, num, glyph, side) {
+  let newString = string;
+  if (typeof string !== "string" || string.trim() === "") {
+    return "Error - Empty or not string";
+  } else if (isNaN(num) || num <= 0) {
+    return "Error - not a number";
+  } else if (glyph.length === 0 || glyph.length > 1) {
+    return "Error - not 1 symbol";
+  } else if (typeof side !== "boolean") {
+    return "Error - not boolean";
+  }
 
-
-
-    if (side) {
-        newString = string.padEnd(num, glyph);
-} else {
-    newString = string.padStart(num, glyph); 
-}
-    
-
-if (string.length > num) {
+  if (string.length > num) {
     newString = newString.substr(0, num);
+  } else if (string.length < num) {
+    if (side) {
+      newString = string.padEnd(num, glyph);
+    } else {
+      newString = string.padStart(num, glyph);
+    }
+  }
+  return newString;
 }
 
-return newString;
-}
-
-let newString = padString ("hello", 8, "*", false);
+let newString = padString("hello", 8, "*", false);
 console.log(newString);
-
